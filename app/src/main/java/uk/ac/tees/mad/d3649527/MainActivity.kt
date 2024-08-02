df306ee00de110e980d4735f476979346152ed78
D3649527 <d3649527@live.tees.ac.uk>
2024-08-02 10:41:47 +0100

    Added Author name

--- app/src/main/java/uk/ac/tees/mad/d3649527/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/d3649527/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import uk.ac.tees.mad.d3649527.ui.theme.ToDoListAppTheme
 
+/**
+ * @author Nikitha
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
